76cea12e71a5d6175ec236ba808d0dd643292eef
Siddique-7 <muhammadsiddik687@gmail.com>
2025-10-13 23:46:29 +0530

    feat: add routing and PrivateRoute setup in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,51 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Tickets from "./pages/Tickets";
+import Ticket from "./pages/Ticket";
+import NewTicket from "./pages/NewTicket";
 
 function App() {
-
   return (
-<div>
-  <h1 className="text-red-500 bg-green-400">Hello from Vite+React 👋</h1>
-</div>
-  )
+    <>
+      <Router>
+        <div className="container mx-auto px-4">
+          <Header />
+
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Protected Routes */}
+            <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/tickets" element={<PrivateRoute><Tickets /></PrivateRoute>} />
+            <Route path="/ticket/:id" element={<PrivateRoute><Ticket /></PrivateRoute>} />
+            <Route path="/new-ticket" element={<PrivateRoute><NewTicket /></PrivateRoute>} />
+          </Routes>
+        </div>
+      </Router>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 }
 
-export default App
+export default App;

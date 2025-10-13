56248175bcbdb720ecb05944ba6d7bd4e637a6ad
Siddique-7 <muhammadsiddik687@gmail.com>
2025-10-13 23:45:25 +0530

    chore: main.jsx refactor for Vite + React 18 with Redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './app/store'; // use default export in your store.js
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);

b4e1d33530f12b1cee48cec5bb01a50bfc7f5889
Siddique-7 <muhammadsiddik687@gmail.com>
2025-10-15 23:26:00 +0530

    fix(Backend): Update userRoute.js for authentication and user handling

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,7 @@ import protect from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', registerUser);
+userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/me', protect, getMe);
 
